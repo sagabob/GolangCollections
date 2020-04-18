2978fe6d78369182488364aa409d64c7f4fe3726
Bob Pham <bobpham.tdp@live.com>
2020-04-18 23:36:32 +1000

    work in progress

---
+++ hackerRank/easy/strangeCounter.go
@@ -0,0 +1,30 @@
+package hackerRank
+
+import "math"
+
+// Complete the angryProfessor function below.
+func strangeCounter(t int64) int64 {
+
+	if t <= 3 {
+		return 3 - t + 1
+	}
+
+	numberPower2 := int64(math.Log2(float64(t)))
+
+	store := make([]int64, numberPower2)
+
+	store[0] = 3
+	var i int64 = 1
+	var starter int64 = 0
+	for ; i < numberPower2; i++ {
+
+		store[i] = int64(3*math.Pow(float64(2), float64(i))) + store[i-1]
+
+		if store[i] >= t {
+			starter = store[i]
+			break
+		}
+	}
+
+	return (starter - t + 1)
+}
