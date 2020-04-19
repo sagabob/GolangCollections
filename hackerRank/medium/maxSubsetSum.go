ab217a3fd0762f2c93dfe3b2f4ba527deb5abdad
Bob Pham <bobpham.tdp@live.com>
2020-04-19 20:42:46 +1000

    work in progress

--- hackerRank/medium/maxSubsetSum.go
+++ hackerRank/medium/maxSubsetSum.go
@@ -6,78 +6,20 @@ import (
 )
 
 //https://www.hackerrank.com/challenges/max-array-sum/
-// need a relook, too slow
+// optimized -> recheck DP
 func maxSubsetSum(arr []int32) int32 {
 
 	arrLength := len(arr)
 
-	maxNumber := arrLength / 2
+	trackArray := make([]int32, arrLength)
+	trackArray[0] = int32(math.Max(float64(arr[0]), 0))
+	trackArray[1] = int32(math.Max(float64(arr[1]), float64(trackArray[0])))
 
-	if arrLength%2 == 1 {
-		maxNumber = arrLength/2 + 1
+	for i := 2; i < arrLength; i++ {
+		trackArray[i] = int32(math.Max(float64(trackArray[i-1]), float64(arr[i]+trackArray[i-2])))
 	}
 
-	indexArray := make([]int32, 3)
-
-	indexArray[1] = math.MinInt32
-
-	fmt.Println("Size -> ", maxNumber)
-
-	for i := 2; i <= maxNumber; i++ {
-		fmt.Println("Starting...", i)
-		indexArray[2] = 0
-		subArray(arr, indexArray, int32(i))
-	}
-	return indexArray[1]
-}
-
-func subArray(arr []int32, indexArray []int32, num int32) {
-
-	if num == 1 {
-
-		maxValue := getMax(arr, indexArray)
-		fmt.Println("Final Pick -> ", maxValue, " num =", num)
-		indexArray[0] = indexArray[0] + maxValue
-
-		if indexArray[0] > indexArray[1] {
-			indexArray[1] = indexArray[0]
-			fmt.Println("Output -> ", indexArray[1])
-		}
-
-		indexArray[0] = 0 //reset
-
-		return
-	}
-
-	starter := indexArray[2]
-
-	for i := starter; i < int32(len(arr)); i++ {
-
-		fmt.Println("Pick -> ", arr[i], " num =", num)
-		indexArray[0] = indexArray[0] + arr[i]
-		indexArray[2] = i + 2
-		subArray(arr, indexArray, num-1)
-	}
-
-}
-
-func getMax(arr []int32, indexArray []int32) int32 {
-
-	starter := indexArray[2]
-
-	var max int32 = math.MinInt32
-	var hasValue bool = false
-	for i := starter; i < int32(len(arr)); i++ {
-		if arr[i] >= max {
-
-			max = arr[i]
-			hasValue = true
-		}
-	}
-	if hasValue {
-		return max
-	}
-	return 0
+	return trackArray[arrLength-1]
 }
 
 func Output_maxSubsetSum() {
