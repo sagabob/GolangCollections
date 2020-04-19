0015121284e63280f6a853a8e8474c83270f58a7
Bob Pham <bobpham.tdp@live.com>
2020-04-19 22:52:42 +1000

    work in progress

--- features/core/closures_01.go
+++ features/core/closures_01.go
@@ -28,4 +28,12 @@ func Closures_Sample01() {
 		fmt.Println("Hello Go from an Anonymous	Function Variable")
 	}
 	hello()
+
+	n1 := incrementCounter()
+	fmt.Println("n1 increment counter #1: ", n1())
+	fmt.Println("n1 increment counter #2: ", n1()) // Notice the second invocation; n1 is called twice, so n1 == 2
+
+	n2 := incrementCounter()                       // New instance of initializedNumber
+	fmt.Println("n2 increment counter #1: ", n2()) // n2 is only called once, so n2 == 1
+	fmt.Println("n1 increment counter #3: ", n1()) // state of n1 is not changed with the n2 calls
 }
